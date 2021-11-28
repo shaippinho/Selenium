16aa797cec7b6ffc8f88465fd0c2fd91493bac57
shaippinho <shaienepardo@gmail.com>
2021-11-27 22:44:43 -0300

    Branch de desenv

---
+++ src/Selenium/Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+
+class HelloSelenium
+{
+    static void Main()
+    {
+        using IWebDriver driver = new EdgeDriver(Environment.CurrentDirectory);
+        WebDriverWait wait = new(driver, TimeSpan.FromSeconds(10));
+        driver.Navigate().GoToUrl("https://www.google.com/ncr");
+        driver.FindElement(By.Name("q")).SendKeys("cheese" + Keys.Enter);
+        wait.Until(webDriver => webDriver.FindElement(By.CssSelector("h3")).Displayed);
+        IWebElement firstResult = driver.FindElement(By.CssSelector("h3"));
+        Console.WriteLine(firstResult.GetAttribute("textContent"));
+    }
+}
